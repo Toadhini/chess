463480e81f5b388a2f68cccdcb079364fa9e8b9d
Bennet <bennethill99@gmail.com>
2025-10-19 18:26:56 -0600

    Addition of response message model for testing use to see if endpoints are working or not.

---
+++ server/src/main/java/model/ResponseMessage.java
@@ -0,0 +1,14 @@
+package model;
+//Message used for testing endpoints whether failure or success
+public class ResponseMessage {
+  private String message;
+
+  public ResponseMessage(String message){
+      this.message = message;
+  }
+
+  public String getMessage(){
+      return message;
+  }
+
+}
