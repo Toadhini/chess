21da8829294d9916a6f79e8bcbdd8badbe50352b
Bennet <bennethill99@gmail.com>
2025-10-19 21:14:43 -0600

    Addition of hashmap storage for data.

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -4,8 +4,8 @@ import model.GameData;
 import java.util.Collection;
 
 public interface GameDAO {
-    int createGame(GameData gameName)throws DataAccessException;
-    GameData getGame(String gameID)throws DataAccessException;
+    int createGame(String gameName)throws DataAccessException;
+    GameData getGame(int gameID)throws DataAccessException;
     Collection<GameData> listGames()throws DataAccessException;
     void updateGame(GameData game)throws DataAccessException;
     void clear()throws DataAccessException;
