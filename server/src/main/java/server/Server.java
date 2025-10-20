4f2531aa06a78da992b2aa830ef63f7ea024a850
Bennet <bennethill99@gmail.com>
2025-10-20 00:09:02 -0600

    DAOs added to Server.java and clear service endpoint added.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,16 +1,38 @@
 package server;
 
 import io.javalin.*;
+import dataaccess.*;
+import service.*;
+import server.handlers.*;
 
 public class Server {
 
     private final Javalin javalin;
 
+    //DAOs
+    private final UserDAO userDAO;
+    private final AuthDAO authDAO;
+    private final GameDAO gameDAO;
+
+    //services
+    private final ClearService clearService;
+
+    //Handlers
+    private final ClearHandler clearHandler;
+
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
+        //DAO initialization
+        userDAO = new MemoryUserDAO();
+        authDAO = new MemoryAuthDAO();
+        gameDAO = new MemoryGameDAO();
+        //Service initialization
+        clearService = new ClearService(userDAO, authDAO, gameDAO);
+        //Handler initialization
+        clearHandler = new ClearHandler(clearService);
         // Register your endpoints and exception handlers here.
-
+        javalin.delete("/db/clear", clearHandler::handle);
     }
 
     public int run(int desiredPort) {
