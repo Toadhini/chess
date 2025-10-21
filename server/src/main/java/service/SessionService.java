30b7622d5972acf02ffb97dba64fcc9a4f598fbb
bennethill <bennethill99@gmail.com>
2025-10-21 09:32:26 -0600

    Small update for SessionService.java.

--- server/src/main/java/service/SessionService.java
+++ server/src/main/java/service/SessionService.java
@@ -1,4 +1,23 @@
 package service;
 
+import model.*;
+import dataaccess.*;
+import org.eclipse.jetty.server.Authentication;
+
+import java.util.Collection;
+
+
 public class SessionService {
+    private final AuthDAO authDAO;
+    private final UserDAO userDAO;
+    private final GameDAO gameDAO;
+
+    public SessionService(AuthDAO authDAO, UserDAO userDAO, GameDAO gameDAO){
+        this.authDAO = authDAO;
+        this.userDAO = userDAO;
+        this.gameDAO = gameDAO;
+    }
+
+
+
 }
