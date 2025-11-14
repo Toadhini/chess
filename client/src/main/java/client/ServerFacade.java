21dcd22de8cfe7cf4a217a21f8b04b7585930778
bennethill <bennethill99@gmail.com>
2025-11-14 15:17:50 -0700

    beginning of creation of makeRequest, will help to make requests to server.

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,4 +1,27 @@
 package client;
 
+import com.google.gson.Gson;
+import model.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.*;
+
 public class ServerFacade {
+    private final String serverUrl;
+    private final Gson gson = new Gson();
+
+    public ServerFacade(int port){
+        this.serverUrl = "http://localhost:" + port;
+    }
+
+    /**
+     * Helper Method for making HTTP requests to the server
+     */
+
+    private <T> T makeRequest(String method, String path, Object requestBody, Class<T> responseClass, String authToken) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        URI uri = URI.create(serverUrl + path);
+
+
+    }
 }
