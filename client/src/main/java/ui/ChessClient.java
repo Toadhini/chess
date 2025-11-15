9d779ec2587f5677bd6a7dae7f1b50a69397e4da
Bennet <bennethill99@gmail.com>
2025-11-14 19:29:27 -0700

    Baseline framework for REPL for client, more function creation needed.

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -21,4 +21,37 @@ public class ChessClient {
         this.scanner = new Scanner(System.in);
     }
 
+    //Main loop for REPL
+
+    public void run(){
+        System.out.println(SET_TEXT_BOLD + "Welcome to my Chess Client!" + RESET_TEXT_BOLD_FAINT);
+        System.out.println("Type 'help' to get started");
+
+       boolean running = true;
+       while(running){
+           try {
+               String prompt = getPrompt(); //Create function
+               System.out.print(prompt);
+
+               String line = scanner.nextLine().trim();
+
+               if(line.isEmpty()){
+                   continue;
+               }
+
+               String result = eval(line); //Create function
+
+               if(result.equals("QUIT")){
+                   running = false;
+               } else if (!result.isEmpty()) {
+                   System.out.println(result);
+               }
+
+           } catch (Exception e){
+               System.out.println(SET_TEXT_COLOR_RED + "ERROR: " + e.getMessage() + RESET_TEXT_COLOR);
+           }
+       }
+       System.out.println("GoodBye! Hope you play again soon!");
+       scanner.close; //Create function
+    }
 }
