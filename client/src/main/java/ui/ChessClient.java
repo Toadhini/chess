a05101eb2590a51a967283cbcb57dbee12826630
Bennet <bennethill99@gmail.com>
2025-11-14 19:10:28 -0700

    Starting of ChessClient.java, creation of variables needed and states.

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,4 +1,24 @@
 package ui;
 
+import client.ServerFacade;
+import java.util.Scanner;
+import static ui.EscapeSequences.*;
+
 public class ChessClient {
+    private final ServerFacade serverFacade;
+    private final Scanner scanner;
+    private String authToken = null;
+    private String username = null;
+    private State state = State.PRELOGIN;
+
+    private enum State{
+        PRELOGIN,
+        POSTLOGIN
+    }
+
+    public ChessClient(int port){
+        this.serverFacade = new ServerFacade(port);
+        this.scanner = new Scanner(System.in);
+    }
+
 }
