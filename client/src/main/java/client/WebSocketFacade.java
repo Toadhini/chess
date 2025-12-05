da0a440b74bfcbde4327c555dd4d8af209242f92
bennethill <bennethill99@gmail.com>
2025-12-05 13:26:07 -0700

    Beginning of WebSocketFAcade, initial variables

--- client/src/main/java/client/WebSocketFacade.java
+++ client/src/main/java/client/WebSocketFacade.java
@@ -2,8 +2,10 @@ package client;
 
 import chess.ChessGame;
 import com.google.gson.Gson;
+import com.sun.nio.sctp.NotificationHandler;
 import commands.UserGameCommand;
 import commands.MakeMoveCommand;
+import jakarta.websocket.ClientEndpoint;
 import messages.ServerMessage;
 import messages.LoadGameMessage;
 import messages.ErrorMessage;
@@ -13,5 +15,17 @@ import chess.ChessMove;
 import javax.websocket.*;
 import java.net.URI;
 
+@ClientEndpoint
 public class WebSocketFacade {
+    private Session session;
+    private final NotificationHandler notificationHandler;
+    private final Gson gson = new Gson();
+
+    public interface NotificationHandler{
+        void onLoadGame(ChessGame game);
+        void onNotifications(String message);
+        void onError(String errorMessage);
+    }
+
+
 }
