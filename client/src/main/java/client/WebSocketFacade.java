aef77da2322265cf50b81ea8b5f4fbb54f7162e4
bennethill <bennethill99@gmail.com>
2025-12-05 12:31:34 -0700

    Creation of WebSocketFacade and assumed needed imports for further implementation.

---
+++ client/src/main/java/client/WebSocketFacade.java
@@ -0,0 +1,17 @@
+package client;
+
+import chess.ChessGame;
+import com.google.gson.Gson;
+import commands.UserGameCommand;
+import commands.MakeMoveCommand;
+import messages.ServerMessage;
+import messages.LoadGameMessage;
+import messages.ErrorMessage;
+import messages.NotificationMessage;
+import chess.ChessMove;
+
+import javax.websocket.*;
+import java.net.URI;
+
+public class WebSocketFacade {
+}
