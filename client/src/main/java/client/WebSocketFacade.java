b6e42f8d99795e1a1f85774538eb074189e51373
Bennet <bennethill99@gmail.com>
2025-12-05 18:15:31 -0700

    Renaming and moving of files to fit autograder requirements

--- client/src/main/java/client/WebSocketFacade.java
+++ client/src/main/java/client/WebSocketFacade.java
@@ -2,12 +2,12 @@ package client;
 
 import chess.ChessGame;
 import com.google.gson.Gson;
-import commands.UserGameCommand;
-import commands.MakeMoveCommand;
-import messages.ServerMessage;
-import messages.LoadGameMessage;
-import messages.ErrorMessage;
-import messages.NotificationMessage;
+import websocket.commands.UserGameCommand;
+import websocket.commands.MakeMoveCommand;
+import websocket.messages.ServerMessage;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.ErrorMessage;
+import websocket.messages.NotificationMessage;
 import chess.ChessMove;
 
 import javax.websocket.*;
